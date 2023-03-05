0aabd69f24f0fb17cd42e5af9dd92ce34605fe90
Douglas <douglas.franco@dzfweb.com.br>
2023-03-05 19:57:43 -0300

    added styles

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigage } from "react-router-dom";
+
+//Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 
-//pages
+//Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar></Navbar>
         <div className='container'>
           <Routes>
             <Route path="/" element={<Home></Home>}></Route>
             <Route path="/about" element={<About></About>}></Route>
           </Routes>
         </div>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className={styles.footer}>
+                <p>DZF Web Blog - 2023 &copy;</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
